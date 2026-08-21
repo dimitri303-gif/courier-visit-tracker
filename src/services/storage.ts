dc0f77c0eb55dddaa8f231acfb7ebce98b2dd0bb
dimitri303 <dimitri@example.com>
2026-08-21 17:29:58 +0300

    feat: target Courier Tracker Backend container-bound script exclusively

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -51,7 +51,7 @@ const KEYS = {
 };
 
 // Дефолтна адреса бекенду (може бути змінена в екрані Debug)
-const DEFAULT_API_URL = 'https://script.google.com/macros/s/AKfycbxjdJlpEcJKnwkNQCNMnOe7BnN1mEH4RSeri0CLKiVdBsvWameSpAvHhAXg06WwO4-Q/exec';
+const DEFAULT_API_URL = 'https://script.google.com/macros/s/AKfycbwobsbpl3llmUB_GwHsZAFc15qlyt75DbzmADrcwqgOKdHWs1Xp9KiXKEls2Qw1DBchuQ/exec';
 
 export const StorageService = {
   async getApiUrl(): Promise<string> {
